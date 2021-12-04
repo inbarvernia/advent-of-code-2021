b51e7bfe3d9604965702678180abd6b98c7ea1df
Inbar Vernia <inbar.vernia@gmail.com>
2021-12-04 21:49:44 +0200

    Solved day 1, part 1 in sonar_sweep.rb

--- day1/sonar_sweep.rb
+++ day1/sonar_sweep.rb
@@ -1,3 +1,14 @@
 def sonar_sweep
-  
+  counter = 0
+  previous_line = nil
+  File.foreach("puzzle_input.txt") do |line|
+    line = line.to_i
+    if !previous_line.nil?
+      if line > previous_line
+        counter += 1
+      end
+    end
+    previous_line = line
+  end
+  counter
 end
